c10e2dd2b2e0494a46927da62d625f27cc1b0ced
fazslgonzer <fazslgonzer@gmail.com>
2024-06-22 02:30:00 +0500

    2nd commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os 
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
