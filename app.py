efa6b109b45a343beeb89896aa37226fc37bf33c
fazslgonzer <fazslgonzer@gmail.com>
2024-06-22 21:13:20 +0500

    env

--- app.py
+++ app.py
@@ -7,3 +7,28 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["CHATGROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+groq_api_key=os.getenv('GROQ_API_KEY')
+
+st.title("LawChat for PAk")
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Llama3-8b-8192")
+
+prompt=ChatPromptTemplate.from_template(
+"""
+Answer the questions based on the provided context only.
+Please provide the most accurate response based on the question
+<context>
+{context}
+<context>
+Questions:{input}
+
+"""
+)
+
